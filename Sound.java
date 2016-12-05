6f63f9ffdc1ee229c0047dde9ee311770744653d
Tehillah Leigh Lapura <tolapura@up.edu.ph>
2016-12-05 23:36:16 +0800

    Delete Sound.java

--- Sound.java
+++
@@ -1,9 +0,0 @@
-package Pong;
-import java.applet.Applet;
-import java.applet.AudioClip;
-
-public class Sound { // this class holds all the sound effects used in the game
-    public static final AudioClip BALL = Applet.newAudioClip(Sound.class.getResource("ball.wav"));
-    public static final AudioClip GAMEOVER = Applet.newAudioClip(Sound.class.getResource("gameover.wav"));
-    //public static final AudioClip BACK = Applet.newAudioClip(Sound.class.getResource("back.wav"));
-}
